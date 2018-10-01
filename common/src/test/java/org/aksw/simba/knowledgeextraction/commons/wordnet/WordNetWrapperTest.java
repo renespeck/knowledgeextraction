f10007fa3110e49bb2be54f5e9e35b79c2bc2ec6
Rene Speck <speckrene@gmail.com>
2018-10-01 22:30:15 +0200

    Updates wordnet.

--- common/src/test/java/org/aksw/simba/knowledgeextraction/commons/wordnet/WordNetWrapperTest.java
+++ common/src/test/java/org/aksw/simba/knowledgeextraction/commons/wordnet/WordNetWrapperTest.java
@@ -1,27 +1,31 @@
 package org.aksw.simba.knowledgeextraction.commons.wordnet;
 
+import java.util.Collection;
+
+import org.aksw.simba.knowledgeextraction.commons.nlp.PartOfSpeech;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class WordNetWrapperTest {
 
   protected static Logger LOG = LogManager.getLogger(WordNetWrapperTest.class);
 
-  // FIXME: test depends on external data.
-  /**
-   * <code>
-    String path = "/home/rspeck/Data/WordNet-3.1";
 
-    &#64;Test
-    public void test() {
-      final WordNetWrapper wordNetWrapper = new WordNetWrapper(path);
 
-      final Collection<String> synonyms = wordNetWrapper.//
-          synonymsWordnet("ceo", PartOfSpeech.NOUN, 10);
+  @Test
+  public void test() {
+    final String file = "/WordNet-3.1";
+    final String path = getClass().getResource(file).getPath();
 
-      synonyms.forEach(LOG::info);
+    final WordNetWrapper wordNetWrapper = new WordNetWrapper(path);
 
-      Assert.assertTrue(synonyms.size() <= 10);
-    }</code>
-   */
+    final Collection<String> synonyms = wordNetWrapper.//
+        synonymsWordnet("ceo", PartOfSpeech.NOUN, 10);
+
+    synonyms.forEach(LOG::info);
+
+    Assert.assertTrue(synonyms.size() <= 10);
+  }
 }
